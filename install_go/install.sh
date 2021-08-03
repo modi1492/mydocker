6ee23e8e0b3f2dd643c2e31b775c8bc2341493ab
modi1492 <modi1492@163.com>
2021-08-03 12:23:20 +0800

    save

---
+++ install_go/install.sh
@@ -0,0 +1,22 @@
+brew install mobile-shell
+sudo apt-get install mosh
+sudo ufw allow 60000:61000/udp
+
+
+
+ssh root@23.105.192.123 -p 26058
+D7VtFQLmfgoF
+
+
+
+wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
+tar -C /usr/local/ -xzf go1.7.1.linux-amd64.tar.gz
+
+ssh-keygen -t rsa -C "modi1492@163.com"
+git clone git@github.com:modi1492/mydocker.git
+
+
+
+
+readlink /proc/6204/ns/uts
+readlink /proc/6208/ns/uts
